bf12638bbd14ef246d1a60d71e36ae5844b57a68
instace1 <chanchus@gmail.com>
2024-07-09 12:48:23 +0530

    bucket ownership

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+#creating s3 bucket
+
 resource "aws_s3_bucket" "mybucket" {
   bucket = var.bucketname
+}
+
+# s3 bucket ownership 
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
