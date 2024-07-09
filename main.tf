416cec7a93709568bfa24e8703200264f3d50c45
instace1 <chanchus@gmail.com>
2024-07-09 17:05:18 +0530

    public access/ACL update

--- main.tf
+++ main.tf
@@ -13,3 +13,28 @@ resource "aws_s3_bucket_ownership_controls" "example" {
     object_ownership = "BucketOwnerPreferred"
   }
 }
+
+# s3 bucket public access block ;
+# make arguments false
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+
+  block_public_acls       = false            
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# s3 bucket public ACL
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.mybucket.id
+  acl    = "public-read"
+}
